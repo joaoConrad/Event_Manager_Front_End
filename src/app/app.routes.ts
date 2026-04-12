9984485bb8776325c3b762645413ff40f1c7e798
HemannB <brunohemann@gmail.com>
2026-04-11 21:10:45 -0300

    [WIP] Add home route and set as default page
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/pages/home/home';
 import { EventList } from './features/events/pages/event-list/event-list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'events', component: EventList }
 ];
