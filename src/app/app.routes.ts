4f18b5800b605bf2e8de0ed5693a0337768be072
HemannB <brunohemann@gmail.com>
2026-04-11 22:45:59 -0300

    [WIP] Add event creation page and connect navigation from home
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/pages/home/home';
 import { EventList } from './features/events/pages/event-list/event-list';
+import { EventCreate } from './features/events/pages/event-create/event-create';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'events', component: EventList }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'events', component: EventList },
+    { path: 'events/new', component: EventCreate }
 ];
---
+++ src/app/features/events/pages/event-create/event-create.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-create',
+  imports: [],
+  templateUrl: './event-create.html',
+  styleUrl: './event-create.css',
+})
+export class EventCreate {}
