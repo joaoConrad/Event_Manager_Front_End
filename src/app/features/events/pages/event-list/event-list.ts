646528e2ed2f946a1bbedfe36361860ebaba4de0
HemannB <brunohemann@gmail.com>
2026-04-16 19:50:17 -0300

    Filter event list by available spots
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/app/features/events/pages/event-list/event-list.ts
+++ src/app/features/events/pages/event-list/event-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { EventService } from '../../../../core/services/event';
 import { EventModel } from '../../../../models/event.model';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css'
 })
@@ -12,6 +13,12 @@ export class EventList {
   events: EventModel[] = [];
 
   constructor(private readonly eventService: EventService) {
-    this.events = this.eventService.getAll();
+    this.events = this.eventService
+      .getAll()
+      .filter(event => event.registeredParticipants < event.maxParticipants);
+  }
+
+  getAvailableSpots(event: EventModel): number {
+    return event.maxParticipants - event.registeredParticipants;
   }
 }
