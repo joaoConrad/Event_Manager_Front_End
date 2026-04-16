582dcd3beeb11fae879768234c2e733bd3b434b2
HemannB <brunohemann@gmail.com>
2026-04-16 20:21:00 -0300

    Add authentication routes (login and register)
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/pages/home/home';
 import { EventList } from './features/events/pages/event-list/event-list';
 import { EventCreate } from './features/events/pages/event-create/event-create';
+import { Login } from './features/auth/pages/login/login';
+import { Register } from './features/auth/pages/register/register';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'events', component: EventList },
-    { path: 'events/new', component: EventCreate }
+    { path: 'events/new', component: EventCreate },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
 ];
