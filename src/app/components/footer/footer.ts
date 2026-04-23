1e5279f95baa50319cdcbea25b150557554acb58
PedroCopette <pedrolixinski2005@gmail.com>
2026-04-23 13:29:17 -0300

    footer html pronto, falta o css apenas

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -3,8 +3,13 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {}
+export class Footer {
+
+  currentYear: number = new Date().getFullYear();
+
+}
